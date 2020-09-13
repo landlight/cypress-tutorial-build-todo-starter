3a7a277877ebb69fc06f87893002746ea1f3c640
landlight <ck.light@gmail.com>
2020-09-13 14:29:56 +0700

    :sparkles: fixtures and commands - learn how to use fixtures - learn how to use commands with or without inputs

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,30 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/index.html',
-  filename: 'index.html',
-  inject: 'body'
+	template: './src/index.html',
+	filename: 'index.html',
+	inject: 'body'
 })
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'app.bundle.js'
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'], exclude: /node_modules/ }
-    ]
-  },
-  devtool: 'source-map',
-  plugins: [HtmlWebpackPluginConfig]
- }
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'app.bundle.js'
+	},
+	module: {
+		loaders: [{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+				exclude: /node_modules/
+			}
+		]
+	},
+	devtool: 'source-map',
+	plugins: [HtmlWebpackPluginConfig]
+}
